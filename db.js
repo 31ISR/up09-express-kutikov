05f612b90c0632644150347be2840b97fca9f978
Pasha <39154112+ktkv419@users.noreply.github.com>
2026-03-23 08:50:28 +0000

    123

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ db.exec(`
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         email TEXT UNIQUE NOT NULL,
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
+        password TEXT NOT NULL
     )
 `)
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const bcr = require("bcryptjs")
 const db = require("./db.js")
 
 const app = express()
@@ -22,33 +23,87 @@ app.get("/users", (req, res) => {
         console.error(error)
         res
             .status(500)
-            .json({error: "Something went wrong"})
+            .json({ error: "Something went wrong" })
     }
 })
 
-app.post("/users", (req, res) => {
+app.post("/registration", (req, res) => {
     try {
-        const { email, name } = req.body
+        const { email, name, password } = req.body
 
-        if (!email || !name)
+        if (!email || !name || !password)
             return res
                 .status(400)
-                .json({error: "Missing fields"})
+                .json({ error: "Missing fields" })
+
+        const salt = bcr.genSaltSync(10)
+
+        const hash = bcr.hashSync(password, salt)
+
         const info = db
-            .prepare("INSERT INTO users (email, name) VALUES(?, ?)")
-            .run(email, name)
+            .prepare("INSERT INTO users (email, name, password) VALUES(?, ?, ?)")
+            .run(email, name, hash)
 
         const user = db
             .prepare("SELECT * FROM users WHERE id = ?")
             .get(info.lastInsertRowid)
 
-        return res.status(201).json(user)
+        const { password: _, ...safeUser } = user
+        return res.status(201).json(safeUser)
     } catch (error) {
         console.error(error)
         res
             .status(500)
-            .json({error: "Something went wrong"})
+            .json({ error: "Something went wrong" })
+    }
+})
+
+app.post("/login", (req, res) => {
+    try {
+        const { password, email } = req.body
+        if (!email || !password)
+            return res
+                .status(400)
+                .json({ error: "Missing fields" })
+
+        const user = db
+            .prepare("SELECT * FROM users WHERE email = ?")
+            .get(email)
+
+        if (!user) return res
+            .status(401)
+            .json({ error: "Wrong password" })
+    } catch (error) {
+        console.error(error)
+        res
+            .status(500)
+            .json({ error: "Something went wrong" })
+    }
+})
+
+app.delete("/users/:id", (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!id) res.status(400)
+            .json({ error: "Missing ID" })
+
+        const query = db
+            .prepare("DELETE FROM users WHERE id = ?")
+            .run(id)
+
+        if (query.changes === 0) return res
+            .status(404)
+            .json({ error: "User not found" })
+
+        return res.status(200).json({ message: "User deleted" })
+    } catch (error) {
+        console.error(error)
+        res
+            .status(500)
+            .json({ error: "Something went wrong" })
     }
 })
 
 app.listen(PORT)
+
